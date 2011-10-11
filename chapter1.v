9e13750e0e61a6dc59435ff01bdc46033ce3c8c7
Matt Gray <matthew.thomas.gray@gmail.com>
2011-10-11 14:49:45 +0100

    named cases

--- chapter1.v
+++ chapter1.v
@@ -324,4 +324,92 @@ Proof.
   intros n m.
   simpl.
   reflexivity. Qed.
-  
+
+(* Case Analysis *)
+
+Theorem plus_1_neq_0 : forall n : nat,
+beq_nat (n + 1) 0 = false.
+Proof.
+intros n.
+destruct n as [| n'].
+reflexivity.
+reflexivity.
+Qed.
+
+Theorem negb_involutive : forall b : bool,
+negb (negb b) = b.
+Proof.
+intros b.
+destruct b.
+reflexivity.
+reflexivity.
+Qed.
+
+(* exercise 1.6.1 *)
+
+Theorem zero_nbeq_plus_1 : forall n : nat,
+  beq_nat 0 (n + 1) = false.
+Proof.
+intros n.
+destruct n as [|n'].
+reflexivity.
+reflexivity.
+Qed.
+
+(* named cases *)
+
+Require String. Open Scope string_scope.
+
+Ltac move_to_top x :=
+  match reverse goal with
+  | H : _ |- _ => try move x after H
+  end.
+
+Tactic Notation "assert_eq" ident(x) constr(v) :=
+  let H := fresh in
+  assert (x = v) as H by reflexivity;
+  clear H.
+
+Tactic Notation "Case_aux" ident(x) constr(name) :=
+  first [
+    set (x := name); move_to_top x
+  | assert_eq x name; move_to_top x
+  | fail 1 "because we are working on a different case" ].
+
+Tactic Notation "Case" constr(name) := Case_aux Case name.
+Tactic Notation "SCase" constr(name) := Case_aux SCase name.
+Tactic Notation "SSCase" constr(name) := Case_aux SSCase name.
+Tactic Notation "SSSCase" constr(name) := Case_aux SSSCase name.
+Tactic Notation "SSSSCase" constr(name) := Case_aux SSSSCase name.
+Tactic Notation "SSSSSCase" constr(name) := Case_aux SSSSSCase name.
+Tactic Notation "SSSSSSCase" constr(name) := Case_aux SSSSSSCase name.
+Tactic Notation "SSSSSSSCase" constr(name) := Case_aux SSSSSSSCase name.
+
+Theorem andb_true_elim1 : forall b c : bool,
+  andb b c = true -> b = true.
+Proof.
+  intros b c H.
+  destruct b.
+  Case "b = true".
+    reflexivity.
+  Case "b = false".
+    rewrite <- H. reflexivity. Qed.
+
+(* exercise 1.6.2 *)
+
+Theorem andb_true_elim2 : forall b c : bool,
+andb b c = true -> c = true.
+Proof.
+intros b c H.
+destruct c.
+Case "c = true".
+reflexivity.
+Case "c = false".
+rewrite <- H.
+destruct b.
+SCase "b = false".
+reflexivity.
+SCase "b = true".
+reflexivity.
+Qed.
+
