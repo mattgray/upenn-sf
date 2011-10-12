127d6f6592c8f9ff0d9c72eb3fb3cb18d8f82a67
Matt Gray <matthew.thomas.gray@gmail.com>
2011-10-12 16:21:33 +0100

    inductive proofs

--- chapter1.v
+++ chapter1.v
@@ -413,3 +413,59 @@ SCase "b = true".
 reflexivity.
 Qed.
 
+(* induction *)
+
+Theorem plus_0_r : forall n : nat, (n + 0) = n.
+Proof.
+intros n.
+induction n as [|n'].
+Case "n = 0". reflexivity.
+Case "n = S n'". simpl. rewrite -> IHn'. reflexivity. Qed.
+
+(* exercise 1.7.1 *)
+
+Theorem mult_0_r : forall n:nat, n * 0 = 0.
+Proof.
+intros n.
+induction n as [|n'].
+Case "n = 0". reflexivity.
+Case "S n' * 0 = 0".
+simpl.
+rewrite -> IHn'.
+reflexivity.
+Qed.
+
+
+Theorem plus_n_Sm : forall n m : nat,
+  S (n + m) = n + (S m).
+Proof.
+intros n m.
+induction n as [|n'].
+Case "n=0".
+reflexivity.
+Case "inductive step".
+simpl.
+rewrite -> IHn'.
+reflexivity.
+Qed.
+
+Theorem plus_comm : forall n m : nat,
+  n + m = m + n.
+Proof.
+intros n m.
+induction n as [|n'].
+Case "n = 0". simpl. rewrite -> plus_0_r. reflexivity.
+Case "inductive". simpl. rewrite -> IHn'. rewrite -> plus_n_Sm. reflexivity.
+Qed.
+
+Fixpoint double (n:nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n .
+Proof.
+  (* tbc *) Qed.
+
+
